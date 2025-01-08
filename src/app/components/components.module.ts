b444508fcb5129e6fbffb75ca1b77113cd004720
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2025-01-08 09:34:42 -0300

    first commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+@NgModule({
+    declarations: [
+        UserDetailsComponent
+    ],
+    imports: [
+        AngularMaterialModule
+    ],
+    exports: [
+        UserDetailsComponent
+    ],
+})
+export class ComponentsModule { }
