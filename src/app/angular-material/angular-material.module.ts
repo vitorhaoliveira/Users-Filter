230eeb066267678cdad950ec7a1a11ee848c5150
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2025-01-08 12:19:14 -0300

    Finalizado layout

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from "@angular/core";
 
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
     imports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatTableModule
     ],
     exports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatTableModule
     ],
 })
 export class AngularMaterialModule { }
