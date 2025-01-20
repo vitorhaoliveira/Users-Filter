aa256707b1662b81346e01cf4ce0abe6570a029a
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2025-01-20 12:47:07 -0300

    ajustes

--- src/app/pipes/address.pipe.ts
+++ src/app/pipes/address.pipe.ts
@@ -2,10 +2,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 import type { IAddress } from '../interfaces/user/address.interface';
 
 @Pipe({
-  name: 'address'
+  name: 'address',
 })
 export class AddressPipe implements PipeTransform {
   transform(address: IAddress): string {
-    return null;
+    const INVALID_ADDRESS =
+      !address ||
+      !address.rua ||
+      !address.cidade ||
+      !address.estado ||
+      !address.numero === null || !address.numero === undefined
+
+      if (INVALID_ADDRESS) {
+        return 'Endereço indisponível ou inválido'
+      }
   }
 }
